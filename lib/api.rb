ec45f30b76f540c08482f11cd1bd757a41dd203d
michaelle <michaelle.barbor@outlook.com>
2020-10-14 20:48:46 -0400

    information added to record

--- lib/api.rb
+++ lib/api.rb
@@ -1,3 +1,27 @@
 class API
 
+def self.fetch_records
+    url = "http://openlibrary.org/api/volumes/brief/isbn/9780525440987.json"
+    
+     uri = URI(url)
+     response = Net::HTTP.get(uri)
+     hash = JSON.parse(response)
+
+     array_of_records = hash[records]
+
+     # :title,  :subjects, :authors, :edition_name
+     
+     
+     array_of_records.each do |record_hash|
+        binding.pry
+    end
+     
+     
+    end 
+    
+
+
+
+
+
 end
--- lib/record.rb
+++ lib/record.rb
@@ -1,3 +1,3 @@
 class Record
-
+    attr_accessor :title,  :subjects, :authors, :edition_name
 end
